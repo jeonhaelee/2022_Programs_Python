6499b35a0c580061be4724c540fdb9df9d739a81
jeonhaelee <jhealee0624@gmail.com>
2022-04-05 22:02:53 +0900

    article 프로그램 만들기_함축,추상화 2

--- article_program.py
+++ article_program.py
@@ -1,91 +1,29 @@
 # 프로그램 기본 구조
-
 # 입력-처리-출력 -> 반복
 
-
-articles = []
-# article 딕셔너리들이 들어갈 articles 리스트.
-
-def print_articles():
-    print("======================================")
-    for i in range(len(articles)):
-        target = articles[i]
-        print("번호 : {}".format(i+1))
-        print("제목 : {}".format(target["title"]))
-        print("======================================")
-
-def add_article():
-    
-    title = input("제목을 입력해주세요 : ")
-    body = input("내용을 입력해주세요 : ")
-        
-    article = {"title" : title, "body" : body}
-    articles.append(article)
-        
-    print("게시물이 저장되었습니다.")
-
-def update_article():
-    no = input("수정할 게시물 번호 : ")
-    no = int(no)
-        
-    target_idx = no-1
-    if target_idx >= len(articles):
-        print("없는 게시물 번호입니다.")
-            
-    else:
-        update_target = articles[target_idx]
-        
-        new_title = input("새제목 : ")
-        new_body = input("새내용 : ")
-        update_target["title"] = new_title
-        update_target["body"] = new_body
-        
-        print("수정이 완료되었습니다.")
-        print_articles()
-
-def delete_article():
-    no = input("삭제할 게시물 번호 : ")
-    no = int(no)
-    
-    target_idx = no-1
-    if target_idx >= len(articles):
-        print("없는 게시물 번호입니다.")
-        
-    else:
-        del articles[target_idx]
-
-        print("삭제가 완료되었습니다.")
-        print_articles()
-        
-def print_help():
-    print("도움말 목록")
-    print("add : 게시물 등록")
-    print("list : 게시물 목록 조회")
-    print("update : 게시물 수정")
-    print("delete : 게시물 삭제")
-        
+import article_mod as am
         
 while True:
     cmd = input("명령어를 입력해주세요 : ")
     
     if cmd == "help":
-        print_help()
+        am.print_help()
         
     elif cmd == "exit":
         print("프로그램을 종료합니다.")
         break
     
     elif cmd == "add":
-        add_article()
+        am.add_article()
         
     elif cmd == "list":
-        print_articles()
+        am.print_articles()
     
     elif cmd == "update":
-        update_article()
+        am.update_article()
     
     elif cmd == "delete":
-        delete_article()
+        am.delete_article()
         
     else :
         print("알 수 없는 명령어. 명령어 목록을 보시려면 help를 입력하세요.")
